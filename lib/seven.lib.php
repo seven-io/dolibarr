1871578bdd9a673567598a6e1d7e770a12f2aaf8
André Matthies <matthiez@gmail.com>
2023-12-05 15:54:19 +0100

    fix module paths

--- lib/seven.lib.php
+++ lib/seven.lib.php
@@ -12,12 +12,14 @@ function sevenAdminPrepareHead() {
     $h = 0;
     $head = [];
 
-    $head[$h][0] = dol_buildpath("/seven/admin/setup.php", 1);
+    //$head[$h][0] = dol_buildpath("/seven/admin/setup.php", 1);
+	$head[$h][0] = dol_buildpath('/custom/seven/admin/setup.php', 1);
     $head[$h][1] = $langs->trans("Settings");
     $head[$h][2] = 'settings';
     $h++;
 
-    $head[$h][0] = dol_buildpath("/seven/admin/about.php", 1);
+    //$head[$h][0] = dol_buildpath("/seven/admin/about.php", 1);
+	$head[$h][0] = dol_buildpath('/custom/seven/admin/about.php', 1);
     $head[$h][1] = $langs->trans("About");
     $head[$h][2] = 'about';
     $h++;
