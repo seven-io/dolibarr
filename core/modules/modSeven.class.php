b304d4263b841180a85bdaff8ebe6671eb193fb3
André Matthies <matthiez@gmail.com>
2023-12-20 15:14:37 +0100

    bump version

--- core/modules/modSeven.class.php
+++ core/modules/modSeven.class.php
@@ -41,7 +41,7 @@ class modSeven extends DolibarrModules {
         $this->editor_url = 'https://www.seven.io';
 
         // Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated' or a version string like 'x.y.z'
-        $this->version = '1.1';
+        $this->version = '2.1.0';
         // Url to the file with your last numberversion of this module
         //$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
